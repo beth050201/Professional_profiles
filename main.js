c7cfa868a74cfe11f9cc6b3367522dcd2366f48c
beth050201 <56397955+beth050201@users.noreply.github.com>
2020-03-06 16:33:19 +0000

    set up

---
+++ main.js
@@ -0,0 +1,7 @@
+function changePic() {
+    document.getElementById("profilePic").src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/594589/adrian-prp.jpg";
+  }
+  
+  function normalPic() {
+    document.getElementById("profilePic") .src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/594589/adrian.webp";
+  }
